d4c403a2eda6bb556574148c0e1a6d1acda84249
Cleber Miranda <team@clebermiranda>
2022-08-23 11:45:43 -0500

    version2.0

--- mayor_numeros.rb
+++ mayor_numeros.rb
@@ -1,20 +1,20 @@
 class Calcular
-  attr_accessor = :num1, :num2, :num3
-  def initialize(num1, num2, num3)
-    @num1,@num2,@num3 = num1, num2,num3
+  attr_accessor :num1, :num2, :num3
+  def initialize
+    @num1, @num2, @num3 = num1, num2,num3
   end
 
   def pedirDatos
     puts "ingrese 3 numeros:"
-    num1 =gets.chomp.to_i
-    num2= gets.chomp.to_i
-    num3= gets.chomp.to_i
+    @num1 =gets.chomp.to_i
+    @num2= gets.chomp.to_i
+    @num3= gets.chomp.to_i
   end
 
   def calcular_mayor
-    if num1 >num2 & num1> num2
+    if num1 >num2 && num1> num2
       puts" mayor: #{num1}"
-    elsif num2> num1 & num2> num3
+    elsif num2> num1 && num2> num3
       puts "mayor:#{num2}"
     else
       puts "mayor: #{num3}"
